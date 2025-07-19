a0e38a878d4a6b944ef03f65bf165be1b03ab24a
Migi <miguelaguilarfere@gmail.com>
2025-07-18 19:12:46 -0500

    cambios de antes

--- src/database/create_db.py
+++ src/database/create_db.py
@@ -1,5 +1,5 @@
-from src.database.models import Base
-from src.database import engine
+from database.models import Base
+from database import engine
 
 if __name__ == '__main__':
     Base.metadata.create_all(bind=engine)
