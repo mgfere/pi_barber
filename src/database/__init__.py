055e1033d4210f9cf7d2d3fb8a86c8e7d34c587e
Migi <miguelaguilarfere@gmail.com>
2025-07-30 00:11:27 -0500

    Deploy MUAA

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,10 +1,13 @@
-# Base de datos MySQL
-import os
+# Base de datos Supabase
+
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv() # Carga las variables del archivo .env
+# Configuración para PostgreSQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carga las variables del archivo .env
 
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -12,7 +15,8 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
-engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+
 
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 metadata = MetaData()
